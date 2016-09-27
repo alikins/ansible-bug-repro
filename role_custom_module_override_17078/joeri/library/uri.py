90d769726ffaf955b7414d4c41f8161fc1fd2435
Adrian Likins <alikins@redhat.com>
2016-09-27 14:49:42 -0400

    add repros for 17078 and 17747

---
+++ role_custom_module_override_17078/joeri/library/uri.py
@@ -0,0 +1,3 @@
+#! /usr/bin/python
+
+print '{"changed": false, "failed": false, "msg": "custom module run"}'
---
+++ role_custom_module_override_17747/roles/foo/library/stat.py
@@ -0,0 +1,3 @@
+#! /usr/bin/python
+
+print '{"changed": false, "failed": false, "msg": "custom module run. This time i am pretending to be a totally different stat module"}'
