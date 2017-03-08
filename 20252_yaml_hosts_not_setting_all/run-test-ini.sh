434ca281d134bdd928d4ea8abd5e19467330d4fe
Adrian Likins <alikins@redhat.com>
2017-03-08 11:37:33 -0500

    update tests to include asserts

--- 20252_yaml_hosts_not_setting_all/run-test-ini.sh
+++ 20252_yaml_hosts_not_setting_all/run-test-ini.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+echo "Running ansible ad hoc with hosts.ini"
 ansible localhost -i hosts.ini -m debug -a var=groups.all -vvvv
+
+echo "Running ansible-playbook with hosts.ini"
+ansible-playbook -vvv -i hosts.ini test.yml
--- 20252_yaml_hosts_not_setting_all/run-test-yaml.sh
+++ 20252_yaml_hosts_not_setting_all/run-test-yaml.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+echo "Running ansible ad hoc with hosts.yaml"
 ansible localhost -i hosts.yml -m debug -a var=groups.all -vvvv
+
+echo "Running ansible-playbook with hosts.yaml"
+ansible-playbook -vvv -i hosts.yml test.yml
